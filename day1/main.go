5589b2de3b4445d9b2370488f05b232d5e53c977
Azaan <azaanwadhwania@yahoo.co.in>
2022-12-11 00:34:40 -0800

    add part 2 solution

--- day1/main.go
+++ day1/main.go
@@ -32,18 +32,57 @@ func main(){
     maxCal := 0
     calBuffer := 0
 
-    for scanner.Scan(){
-        lineText := scanner.Text()
-        if(lineText != ""){ 
-            perLineCal, _ := strconv.Atoi(lineText)
-            calBuffer += perLineCal
-            if(calBuffer > maxCal){
-                maxCal = calBuffer
+    first := 0
+    second := 0
+    third := 0
+
+  //  for scanner.Scan(){
+  //      lineText := scanner.Text()
+  //      if(lineText != ""){ 
+  //          perLineCal, _ := strconv.Atoi(lineText)
+  //          calBuffer += perLineCal
+  //          if(calBuffer > maxCal){
+  //              maxCal = calBuffer
+  //          }
+  //      }else if(lineText == ""){
+  //          calBuffer = 0;
+  //      }
+  //  }
+for scanner.Scan(){
+    lineText := scanner.Text()
+    if(lineText != ""){
+        perLineCal, _ := strconv.Atoi(lineText)
+        calBuffer += perLineCal
+        
+    }else if(lineText == ""){
+        fmt.Println(calBuffer)
+        fmt.Println("THIS IS...", first, second, third)
+        if(calBuffer > first){
+            if(first > second){
+                if(second > third){
+                    third = second
+                }
+                second = first
             }
-        }else if(lineText == ""){
-            calBuffer = 0;
+            first = calBuffer
+        }else if(calBuffer > second){
+            if(second > third){
+                third = second
+            }
+            second = calBuffer
+        }else if(calBuffer > third){
+            third = calBuffer
         }
+        calBuffer = 0;
     }
+}
+
+fmt.Println("THIS IS...", first, second, third)
+total := 0
+total = first + second + third
+fmt.Println("THE TOTAL IS:")
+fmt.Println(total)
+fmt.Println("----------------------------------")
 
     fmt.Println(maxCal)
 
